028797eac32d96305ccd2c5e7f91c6d19e72f4ce
Esmaeil MIRZAEE <esmaeilmirzaee@gmail.com>
2020-10-28 13:34:14 -0400

    Section 06 | math operations

---
+++ module-01/section_06.js
@@ -0,0 +1,24 @@
+// Math operations
+const numOne = 23;
+const numTwo = 32;
+let result;
+
+// simple math with numbers
+result = numOne + numTwo;
+result = numOne - numTwo;
+result = numOne * numTwo;
+result = numOne / numTwo;
+
+// math object
+console.log(Math.PI);
+console.log(Math.E);
+console.log(Math.round(2.4));
+console.log(Math.floor(2.9999));
+console.log(Math.ceil(2.0000001));
+console.log(Math.sqrt(100));
+console.log(Math.abs(-100));
+console.log(Math.pow(8, 2));
+console.log(Math.min(0, 1, 2, 3, 4, -9, 10, 13));
+console.log(Math.max(0, 1, 2, 3, 4, -9, 10, 13));
+console.log(Math.random());
+console.log(Math.floor(Math.random() * 20 + 1));
