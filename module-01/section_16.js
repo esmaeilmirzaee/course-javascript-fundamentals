503a93e2e2e600b11d98418508c26754433b4e98
Esmaeil MIRZAEE <esmaeilmirzaee@gmail.com>
2020-10-30 17:16:46 -0400

    Section 16 | Window object

--- module-01/section_16.js
+++ module-01/section_16.js
@@ -1,2 +1,48 @@
 // Module 01 | Section 16
 // Window object
+// Alert
+// alert('Welcome');
+
+// Confirm
+// if (confirm('Are you sure you want to quit?')) {
+//   console.log('YES');
+// } else {
+//   console.log('NO');
+// }
+
+// Prompt
+// let userInput = prompt('Please enter your name: ');
+// alert(`Welcome ${userInput}`);
+
+// outer size
+console.log(window.outerHeight);
+console.log(window.innerWidth);
+
+// inner size
+console.log(window.innerHeight);
+console.log(window.innerWidth);
+
+// Scroll
+res = window.scrollX;
+res = window.scrollY;
+
+res = window.location;
+res = window.location.hostname;
+res = window.location.port;
+res = window.location.href;
+res = window.location.search;
+
+// Redirect
+window.location.href = 'https://google.com';
+
+// Reload
+window.location.reload();
+
+// Navigator object
+res = window.navigator;
+res = window.navigator.appName;
+res = window.navigator.appVersion;
+res = window.navigator.geolocation;
+res = window.navigator.platform;
+
+console.log(res);
