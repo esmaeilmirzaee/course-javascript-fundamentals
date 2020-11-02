5bb9e84bf8d6a6396a87bb1fd4f9ae6f6a4b3884
Esmaeil MIRZAEE <esmaeilmirzaee@gmail.com>
2020-11-02 18:03:19 -0500

    Module 02 | Section 09 | Keyboard event

---
+++ module-02/section_09/app.js
@@ -0,0 +1,43 @@
+/*
+ * Module 02 | Section 07
+ * Keyvoard Events
+ */
+const form = document.querySelector('form');
+const taskInput = document.getElementsByClassName('newTask')[0];
+const heading = document.querySelector('h6');
+
+// Clear input
+console.log(taskInput);
+taskInput.value = '';
+// form.addEventListener('submit', eventType);
+// keydown
+taskInput.addEventListener('keydown', eventType);
+
+// keyup
+taskInput.addEventListener('keyup', eventType);
+
+// keypress
+taskInput.addEventListener('keypress', eventType);
+
+// Focus
+taskInput.addEventListener('focus', eventType);
+
+// blur
+taskInput.addEventListener('blur', eventType);
+
+// cut
+taskInput.addEventListener('cut', eventType);
+
+// paste
+taskInput.addEventListener('paste', eventType);
+
+// Input
+taskInput.addEventListener('input', eventType);
+
+function eventType(e) {
+  console.log(`EVENT TYPE: ${e.type}`);
+
+  console.log(e.target.value);
+  heading.innerText = e.target.value;
+  // e.preventDefault(); // remove to make the screen responsible when keys pressed
+}
