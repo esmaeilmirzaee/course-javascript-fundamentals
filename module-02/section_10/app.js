bd44156328981397953cc4b2f22c9a5f3ee50fc1
Esmaeil MIRZAEE <esmaeilmirzaee@gmail.com>
2020-11-05 07:40:14 -0500

    Module 02 | Section 10 | Event bubbling and delegation

--- module-02/section_10/app.js
+++ module-02/section_10/app.js
@@ -2,23 +2,23 @@
  * Module 02 | Section 10
  * Event bubbling and event delegation
  */
-// document.querySelector('.newTask').addEventListener('click', function (e) {
-//   console.log('newTask');
-// });
+document.querySelector('.newTask').addEventListener('click', function (e) {
+  console.log('newTask');
+});
 
-// document
-//   .querySelector('.row.task__item')
-//   .addEventListener('click', function (e) {
-//     console.log('row task__item');
-//   });
+document
+  .querySelector('.row.task__item')
+  .addEventListener('click', function (e) {
+    console.log('row task__item');
+  });
 
-// document.querySelector('.row').addEventListener('click', function (e) {
-//   console.log('row');
-// });
+document.querySelector('.row').addEventListener('click', function (e) {
+  console.log('row');
+});
 
-// document.querySelector('.container').addEventListener('click', function (e) {
-//   console.log('container');
-// });
+document.querySelector('.container').addEventListener('click', function (e) {
+  console.log('container');
+});
 
 // Event Delegation
 
