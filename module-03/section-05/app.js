c92821c6be27fae35a5a6f52eb701b972c3f2e40
Esmaeil MIRZAEE <esmaeilmirzaee@gmail.com>
2020-11-08 03:18:39 -0500

    Module 03 | Section 07 | App functionality

--- module-03/section-05/app.js
+++ module-03/section-05/app.js
@@ -1,7 +1,59 @@
-const min = 0
-const max = 100
+// Variables
+let indefinitely = false;
+const min = 0;
+const max = 100;
+let randomInt;
 
-const tickUI = document.getElementById('tick');
-tickUI.style.display = 'none';
-document.getElementById('message').style.display = 'none'
-document.getElementById('guess').placeholder = `${min} ~ ${max}`
+
+// UI variables
+const guessUI = document.querySelector("#guess");
+const messageUI = document.querySelector("#message");
+const indefinitelyUI = document.querySelector("#indefinitely");
+const buttonUI = document.querySelector("#checkBtn");
+const replayButtonUI = document.querySelector("#replay");
+
+document.addEventListener('DOMContentLoaded', function(e) {
+    generateRandomInt();
+    replayButtonUI.style.display = 'none';
+    messageUI.style.display = 'none';
+});
+
+indefinitelyUI.addEventListener("change", function(e) {
+    indefinitely = !indefinitely;
+});
+
+buttonUI.addEventListener("click", function(e) {
+    if (!isNaN(parseInt(guessUI.value))) {
+        checkTheAnswer(randomInt, parseInt(guessUI.value));
+    }
+});
+
+replayButtonUI.addEventListener("click", function(e) {
+    resetTheGame();
+})
+
+function checkTheAnswer(random, guess) {
+    console.log(random, guess);
+    if (random === guess) {
+        buttonUI.classList.add('is-light');
+        buttonUI.children[1].style.display = 'none';
+        buttonUI.disabled = true;
+        guessUI.disabled = true;
+        guessUI.parentElement.children[2].classList.add('has-text-primary');
+        messageUI.innerText = "You won.";
+        messageUI.classList.add("has-text-primary");
+        messageUI.style.display = 'block';
+        replayButtonUI.style.display = 'block';
+    } else {
+        generateRandomInt();
+    }
+}
+
+function generateRandomInt() {
+    randomInt = parseInt(Math.random() * max + min);
+    console.log(randomInt);
+}
+
+function restTheGame() {
+    
+}
