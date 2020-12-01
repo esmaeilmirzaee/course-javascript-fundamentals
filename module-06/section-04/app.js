ceecdb774ae96e5c4632e4a10bcd87f4fbc6bf6d
Esmaeil MIRZAEE <esmaeilmirzaee@gmail.com>
2020-12-01 10:12:55 -0500

    Callback functions--recording

--- module-06/section-04/app.js
+++ module-06/section-04/app.js
@@ -1,8 +1,22 @@
-const posts = [
-    {title: 'Post one', body: 'This is a post one.'},
-    {title: 'Post two', body: 'This is post two.'}
-];
+posts = [{title: 'Post one', body: 'This is post one.'}];
+posts.push({title: 'Post two', body: 'This is post two.'});
+
+function createAPost(post, clb) {
+    setTimeout(function() {
+        posts.push(post);
+        clb();
+    }, 2000);
+}
 
 function getPosts() {
-
+    setTimeout(function() {
+        let output = '';
+        posts.forEach(function(post) {
+            output += `<li>${post.title}->${post.body}</li>`;
+        });
+        document.querySelector('.root').innerHTML = output;
+    }, 1000);
 }
+
+createAPost({title: 'Post three', body: 'This is post three.'}, getPosts);
+// getPosts();
